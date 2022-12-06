135d82596a67c11a04b3114bfd904c4d02a4e7d5
ritamirgorodska <ritamirgorodska.1604@gmail.com>
2022-12-06 13:36:36 +0300

    do exercise 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formElement = document.querySelector('.form');
+formElement.addEventListener('submit', onAddPromise);
+function onAddPromise(e) {
+  e.preventDefault();
+
+  let delay = Number(formElement.delay.value);
+  let position = Number(formElement.amount.value);
+  let step = Number(formElement.step.value);
+
+  for (let i = 1; i <= position; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  const form = {
+    position,
+    delay,
+  };
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(form);
+      } else {
+        reject(form);
+      }
+    }, delay);
+  });
 }
